7aee39452fd1fa82ef1b75b5357f605426c2c6e9
Fiza Sayyed <fizaasayyed7@gmail.com>
2022-11-06 21:55:15 +0530

    Added building colours

--- src/index.js
+++ src/index.js
@@ -18,6 +18,21 @@ const dLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
 light.add(dLight);
 scene.add(light);
 
+// Pick a random value from an array for the buildings
+function pickRandom(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
+const buildingColors = [
+	0xef2d56,
+	0x0ad3ff,
+	0xff9f1c, 
+	0xff80ed,
+	0xffd700,
+	0x40e0d0,
+	0x800000,
+	0x00ff7f
+  ];
 // Ground =>
 const geometry = new THREE.BoxGeometry(50,0.1,50);
 const material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
@@ -45,13 +60,13 @@ connect.then((result) =>{
 	console.log(result);
 	result.buildings.forEach((b, index) => {
 		if(index <= result.supply) {
+			// Adding colors from array =>
+			const color = pickRandom(buildingColors);
 			const boxGeometry = new THREE.BoxGeometry(b.w, b.h, b.d);
-			const boxMaterial = new THREE.MeshPhongMaterial( { color: 0xff6666 } );
+			const boxMaterial = new THREE.MeshPhongMaterial( { color } );
 			const box = new THREE.Mesh( boxGeometry, boxMaterial );		
 			box.position.set(b.x, b.y, b.z);
 			scene.add( box );
-			
-
 		}
 		
 	});
